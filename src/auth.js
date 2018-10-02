87c941d0a63c5dff215627f5cd271d956f30f95a
Sindre Slungaard <slungaards@gmail.com>
2018-10-02 10:28:58 +0200

    store decoded jwt token data

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,12 @@
 import query from 'query-string';
+import jwt from 'jwt-decode';
 
 export default class Authenticator {
 
   static authenticate(opts) {
-    Authenticator.token = opts.token;
-    Authenticator.namespace = opts.namespace;
+    this.token = opts.token;
+    this.namespace = opts.namespace;
+    this.tokenData = Authenticator.parseToken(opts.token);
   }
 
   static parseUrl() {
@@ -16,9 +18,14 @@ export default class Authenticator {
     if(!urlParams.access_token) throw new Error("Could not find the parameter access_token in URL");
 
     this.token = urlParams.access_token;
+    this.tokenData = this.parseToken(urlParams.access_token);
 
     if(urlParams.c) this.namespace = urlParams.c;
 
+  }
+
+  static parseToken(token) {
+    return jwt(token);
   }
 
 }
